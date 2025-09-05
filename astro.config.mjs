8dbedb8ce84e030d2d76865e9798fd2457bfeed5
alexauron <sevillaciudaddelbetis@gmail.com>
2025-09-05 12:53:32 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,24 @@
-import { defineConfig } from 'astro/config';
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-// https://astro.build/config
-export default defineConfig({});
+const AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE";
+
+app.use(bodyParser.json());
+
+app.get('/api/eval', (req, res) => {
+  const code = req.query.code;
+  eval(code);
+  res.send('Código ejecutado.');
+});
+
+app.post('/api/deserialize', (req, res) => {
+  const serialized = req.body.data;
+  // Deserialización de datos no confiables
+  const obj = JSON.parse(serialized);
+  res.json({ message: 'Objeto deserializado.', data: obj });
+});
+
+app.listen(3000, () => {
+  console.log('Servidor escuchando en el puerto 3000');
+});
